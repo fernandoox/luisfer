003490e3c6d3b4a54a996affb2bcd11824cbea6c
Luis Fernando Fz <luis.fernan.fz@gmail.com>
2025-03-12 14:23:53 -0600

    feat: update homepage content to include personal introduction and experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,23 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Bienvenido a mi Portfolio</h1>
-      <p>Explora mis proyectos destacados:</p>
-
+    <div className="w-full md:w-2/5 mx-auto p-6">
+      <p className="mb-4">
+        Hola! 👋 I am a software engineer and problem solver with a passion for
+        technology, currently writing solutions for MaxiSend. I love developing
+        front-end applications using React & Angular, among other technologies –
+        see my full stack [link].
+      </p>
+      <p className="mb-4">
+        I have previously worked at Cornershop by Uber and later at Uber, and I
+        also have experience collaborating with companies in diverse sectors
+        such as insurance, banking/fintech, retail, and remittances.
+      </p>
+      <p className="mb-4">
+        At Badak, as an engineering lead, I designed and implemented a training
+        program for new interns. This initiative was created to help fresh
+        talent start their careers within the company smoothly and effectively.
+      </p>
       {/* Botón de navegación a la página de proyectos */}
       <AnimatedDiv
         initial={{ opacity: 0 }}
